87a585b32d50bbc9ed48bce7b16393da9f2c254e
Juan Ortiz <juan.ortiz@konexinnovation.com.co>
2024-05-13 09:49:47 -0500

    feature: Creación base de microservicios, proyecto

---
+++ ms-companies/src/main/java/com/jjos/mscompanies/MsCompaniesApplication.java
@@ -0,0 +1,13 @@
+package com.jjos.mscompanies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompaniesApplication.class, args);
+	}
+
+}
