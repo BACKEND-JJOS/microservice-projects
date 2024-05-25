101f0c501d33759bee2513e9d7d0dd3a1633af03
Juan Ortiz <juan.ortiz@konexinnovation.com.co>
2024-05-25 10:51:48 -0500

    feature: Adding cloud stream with kafka, zookeper, adding mongo for persist report

---
+++ ms-report-listener/src/main/java/com/jjos/msreportlistener/MsReportListenerApplication.java
@@ -0,0 +1,13 @@
+package com.jjos.msreportlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReportListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReportListenerApplication.class, args);
+	}
+
+}
---
+++ ms-report-listener/src/test/java/com/jjos/msreportlistener/MsReportListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjos.msreportlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
