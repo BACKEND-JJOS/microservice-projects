748ad6706ca7cbfca25bc1061964738ec7750e3c
Juan Ortiz <juan.ortiz@konexinnovation.com.co>
2024-06-09 09:51:11 -0500

    feature: Agregando nuevo microservicio para autenticación

---
+++ ms-auth-server/src/test/java/com/jjos/ms_auth_server/MsAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjos.ms_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
